fd599e723c066d434d5581b793308782e33692e4
unnamed <arrynpal@am-g1.msk.21-school.ru>
2022-02-02 19:49:01 +0300

    my

--- src/arithmetic.c
+++ src/arithmetic.c
@@ -1,12 +1,36 @@
 #include <stdio.h>
 
 int sum(int a, int b);
+int dif(int a, int b);
+int mul(int a, int b);
+int pri(int a, int b);
 
 int main()
 {
-    return 0;
+	int x, y;
+	scanf("%d %d", &x, &y);
+	if (y == 0)
+	{
+		printf("%d %d %d n/a", sum(x, y), dif(x,y), mul(x,y));
+	}else{
+		printf("%d %d %d %d", sum(x, y), dif(x, y), mul(x,y), pri(x, y));
+	}
 }
 
 int sum(int a, int b){
-    return (2 * a * b) / (a - b);
+    return a + b;
 }
+
+int dif(int a, int b){
+    return a - b;
+}
+
+int mul(int a, int b){
+    return a * b;
+}
+
+int pri(int a, int b){
+    return a / b;
+}
+
+
